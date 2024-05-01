646321d7fcc614886fd26da9292d2491c4167b54
Hauke Rehfeld <git@haukerehfeld.de>
2024-05-01 13:08:50 +0200

    chg: save models.json instead of hardcoding

--- generate.py
+++ generate.py
@@ -1,22 +1,12 @@
 #!/bin/python3
 from pathlib import Path
 import os
+import json
 
 _pkgbase = "whisper.cpp-model"
 
-models = {
-    'tiny': 'bd577a113a864445d4c299885e0cb97d4ba92b5f',
-    'tiny.en': 'c78c86eb1a8faa21b369bcd33207cc90d64ae9df',
-    'base': '465707469ff3a37a2b9b8d8f89f2f99de7299dac',
-    'base.en': '137c40403d78fd54d454da0f9bd998f78703390c',
-    'small': '55356645c2b361a969dfd0ef2c5a50d530afd8d5',
-    'small.en': 'db8a495a91d927739e50b3fc1cc4c6b8f6c2d022',
-    'medium': 'fd9727b6e1217c2f614f9b698455c4ffd82463b4',
-    'medium.en': '8c30f0e44ce9560643ebd10bbe50cd20eafd3723',
-    'large-v1': 'b1caaf735c4cc1429223d5a74f0f4d0b9b59a299',
-    'large-v2': '0f4c8e34f21cf1a914c59d8b3ce882345ad349d6',
-    'large-v3': 'ad82bf6a9043ceed055076d0fd39f5f186ff8062',
-}
+with open('models.json', 'r') as f:
+    models = json.load(f)
 
 extra_variables = {
     'large-v2': {
